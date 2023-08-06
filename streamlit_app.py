8661f8234fbd2db045bc1807847ac95b4cd11f16
anand86 <41064690+anand86@users.noreply.github.com>
2023-08-06 17:24:45 -0400

    Update streamlit_app.py
    Add new fruits feature added to the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,11 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What would you like to add?')
+my_cur.execute("INSERT into fruit_load_list values (" + add_my_fruit + ")")
+streamlit.text('Thank your for adding' + add_my_fruit)
